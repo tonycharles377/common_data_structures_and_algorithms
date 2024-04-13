bf2abae012cd095796a2d5bc994efa4a23254112
tonycharles377 <tonycharles377@gmail.com>
2024-04-13 18:51:21 +0300

    Append method

--- linked_list.rb
+++ linked_list.rb
@@ -14,6 +14,23 @@ class LinkedList
     def initialize(first_node)
         @first_node = first_node
     end
+
+    # Methods
+    # Add new node to the end of list
+    def append(value)
+        # Create new node
+        new_node = Node.new(value)
+
+        # Find the last node
+        current_node = first_node
+        
+        while current_node.next_node != nil
+            current_node = current_node.next_node
+        end
+
+        # Append new node
+        current_node.next_node = new_node
+    end
 end
 
 node1 = Node.new('once')
@@ -26,3 +43,7 @@ node2.next_node = node3
 node3.next_node = node4
 
 list = LinkedList.new(node1)
+
+list.append('in Africa')
+
+p list
