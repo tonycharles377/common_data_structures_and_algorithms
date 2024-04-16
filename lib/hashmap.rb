cf9098ab5b6571ddfd173bb22e2417e5ad10d093
tonycharles377 <tonycharles377@gmail.com>
2024-04-16 22:01:46 +0300

    Hash method

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -9,6 +9,17 @@ class HashMap
         @arr_capacity = 0
         @arr = Array.new(arr_size) {[]}
     end
+
+    # Hash method
+    def hash(key)
+        hash_code = 0
+        prime_number = 31
+           
+        key.each_char do  |char|
+            hash_code = prime_number * hash_code + char.ord
+        end  
+        hash_code
+    end     
 end
 
 hashmap = HashMap.new
